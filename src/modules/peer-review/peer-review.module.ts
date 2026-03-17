26add47b67eac975422ca800fd49f07efa6af456
AmirHamdi21 <am3566146@gmail.com>
2026-03-17 03:16:34 +0200

    all sprint 4 dev c endpoints works

--- src/modules/peer-review/peer-review.module.ts
+++ src/modules/peer-review/peer-review.module.ts
@@ -4,10 +4,12 @@ import { PeerReview } from './entities/peer-review.entity';
 import { AssignmentSubmission } from '../assignments/entities/assignment-submission.entity';
 import { PeerReviewService } from './services/peer-review.service';
 import { PeerReviewController } from './controllers/peer-review.controller';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([PeerReview, AssignmentSubmission]),
+    NotificationsModule,
   ],
   controllers: [PeerReviewController],
   providers: [PeerReviewService],
--- src/modules/study-groups/study-groups.module.ts
+++ src/modules/study-groups/study-groups.module.ts
@@ -4,10 +4,12 @@ import { StudyGroup } from './entities/study-group.entity';
 import { StudyGroupMember } from './entities/study-group-member.entity';
 import { StudyGroupsService } from './services/study-groups.service';
 import { StudyGroupsController } from './controllers/study-groups.controller';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([StudyGroup, StudyGroupMember]),
+    NotificationsModule,
   ],
   controllers: [StudyGroupsController],
   providers: [StudyGroupsService],
